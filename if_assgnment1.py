d4c0ef23c10b79a28bb5f2c454959d2927824e12
Betul <betul.nalan@gmail.com>
2020-04-16 23:20:39 -0400

    First commit for python assignment

---
+++ if_assgnment1.py
@@ -0,0 +1,6 @@
+name=input("Please enter your name: ")
+my_name="Betul"
+if name==my_name:
+    print("Hello, Betul! The password is: willpower ")
+else:
+    print("Hello, {}! See you later.".format(name))
