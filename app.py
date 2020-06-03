3a1647f1cdfecf332bc60cda4b17a69d29d44fdd
Betul <betul.nalan@gmail.com>
2020-06-02 20:11:25 -0400

    Modified app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, json, request
-api = Flask(__name__)
-def convert_Roman(number)
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def convert_Roman():
+number=request.form['number']
     dict={1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L",40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"}
     roman=""
     for key in dict.keys():
         count=number//key
         roman+=dict[key]*count
         number-=key*count
-    convert_Roman(number)
-
-@api.route('/', methods=['POST'])
-def convert_Roman:
-  return json.dumps(number)
+    return(roman)
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=80)
