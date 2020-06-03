0d45d277910fe1aa66eeed59926eaa99d44d3e2a
Betul <betul.nalan@gmail.com>
2020-06-02 22:35:01 -0400

    Request.form['integer']

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def convert_Roman():
-number=request.form['number']
+number=request.form['integer']
     dict={1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L",40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"}
     roman=""
     for key in dict.keys():
