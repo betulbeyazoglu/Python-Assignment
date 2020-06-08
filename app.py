928f76a35abe4410678d6070c4d796e01fbfaac3
Betul <betul.nalan@gmail.com>
2020-06-08 12:42:33 -0400

    Last commit

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def convert_Roman():
-number=request.form['integer']
+def convert_Roman(number):
     dict={1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L",40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"}
     roman=""
     for key in dict.keys():
@@ -12,6 +10,12 @@ number=request.form['integer']
         roman+=dict[key]*count
         number-=key*count
     return roman
-    
+
+@app.route('/', methods=['POST'])
+
+def convert():
+  number=request.form['integer']  
+  convert_Roman(number)   
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
